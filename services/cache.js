c0bb2e4c5b852a2ee7421960c522210a278b9fda
ajay <ajayjarwal44@gmail.com>
2023-02-09 14:48:05 +0530

    implement redis cache

---
+++ services/cache.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+
+client.hget = util.promisify(client.hget);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
+  return this; // it helps in method chaining
+};
+
+//This function execute right before query execute.
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  const cachValue = await client.hget(this.hashKey, key);
+
+  if (cachValue) {
+    console.log("LOADING FROM CACHE");
+    const doc = JSON.parse(cachValue);
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments); // returns mongoose document, not JSON object
+  console.log("LOADING FROM DB");
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
+};
